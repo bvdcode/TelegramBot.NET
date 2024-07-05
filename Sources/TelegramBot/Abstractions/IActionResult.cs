fdfd8c7ce40eed6a0e938212c242feed54c6db01
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-07-05 03:41:10 -0700

    chore: Add Handle property to IActionResult interface

--- Sources/TelegramBot/Abstractions/IActionResult.cs
+++ Sources/TelegramBot/Abstractions/IActionResult.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Telegram.Bot;
 
 namespace TelegramBot.Abstractions
 {
     public interface IActionResult
     {
+        Action<ITelegramBotClient> Handle { get; }
     }
 }
