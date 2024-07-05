30ede87eecd9dfb52e877ff879f40675b5a6220b
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-07-05 02:48:36 -0700

    chore: Refactor TelegramBot code structure and dependencies

--- Sources/TelegramBot/BotApp.cs
+++ Sources/TelegramBot/BotApp.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
+using TelegramBot.Controllers;
 
 namespace TelegramBot
 {
+    /// <summary>
+    /// Telegram bot application.
+    /// </summary>
     public class BotApp : IBot
     {
+        /// <summary>
+        /// Maps controllers inherited from <see cref="BotControllerBase"/>.
+        /// </summary>
         public void MapControllers()
         {
-            throw new NotImplementedException();
+
         }
 
+        /// <summary>
+        /// Runs the bot.
+        /// </summary>
+        /// <param name="token">Cancellation token (optional).</param>
         public void Run(CancellationToken token = default)
         {
-            throw new NotImplementedException();
+
         }
     }
 }
--- Sources/TelegramBot/IBot.cs
+++ Sources/TelegramBot/IBot.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
+using TelegramBot.Controllers;
 
 namespace TelegramBot
 {
+    /// <summary>
+    /// Interface for bot.
+    /// </summary>
     public interface IBot
     {
+        /// <summary>
+        /// Maps controllers inherited from <see cref="BotControllerBase"/>.
+        /// </summary>
         void MapControllers();
+
+        /// <summary>
+        /// Runs the bot.
+        /// </summary>
+        /// <param name="token">Cancellation token (optional).</param>
         void Run(CancellationToken token = default);
     }
 }
