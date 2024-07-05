4d7e98a3d776e1c0b2d30d843048b67b650e703c
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-07-05 02:46:32 -0700

    chore: Update Telegram Bot builder to use custom Telegram API URL

--- Sources/TelegramBot/Builders/ITelegramServerBuilder.cs
+++ Sources/TelegramBot/Builders/ITelegramServerBuilder.cs
@@ -1,9 +1,18 @@
 ﻿namespace TelegramBot.Builders
 {
+    /// <summary>
+    /// The builder for the Telegram server API.
+    /// </summary>
     public interface ITelegramServerBuilder
     {
+        /// <summary>
+        /// Set the base URL for the Telegram API.
+        /// </summary>
         string BaseUrl { get; set; }
 
+        /// <summary>
+        /// Use the configuration value CustomTelegramApiUrl as the base URL.
+        /// </summary>
         void FromConfiguration();
     }
 }
