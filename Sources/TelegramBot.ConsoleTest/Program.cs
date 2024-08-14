367b0802f76e87838c03be036012c6fe51509317
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-13 19:05:11 -0700

    Add support for Russian bot command registration
    Previously, bot commands were registered only in English. This update adds a new code block to register the same set of commands with their descriptions in Russian, enabling bilingual support for bot commands.

--- Sources/TelegramBot.ConsoleTest/Program.cs
+++ Sources/TelegramBot.ConsoleTest/Program.cs
@@ -19,7 +19,15 @@ namespace TelegramBot.ConsoleTest
                         .RegisterCommand("/burgers", "shows burgers menu")
                         .RegisterCommand("/burgersdone", "notifies that the order is ready")
                         .RegisterCommand("/receipt", "shows the receipt");
-                }, "en");
+                }, "en")
+                .RegisterCommands(x =>
+                {
+                    x.RegisterCommand("/start", "инициирует бота")
+                        .RegisterCommand("/help", "показывает сообщение справки")
+                        .RegisterCommand("/burgers", "показывает меню бургеров")
+                        .RegisterCommand("/burgersdone", "уведомляет о готовности заказа")
+                        .RegisterCommand("/receipt", "показывает чек");
+                }, "ru");
 
             builder.Services
                 .AddDbContext<AppDbContext>(x => x.UseSqlite("Data Source=app.db"))
