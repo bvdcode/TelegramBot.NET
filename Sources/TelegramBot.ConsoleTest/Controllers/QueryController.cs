232c353624caacfa9b6fe330615f140235a35651
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-14 13:00:12 -0700

    Add QueryController for handling specific text queries
    Introduced a new `QueryController` class in the `TelegramBot.ConsoleTest.Controllers` namespace. This class inherits from `BotControllerBase` and includes two methods, `HandleHelloAsync` and `HandleByeAsync`, which handle text queries matching "hello" and "bye" patterns, respectively. The class uses attributes from the `TelegramBot` namespace to define these text query patterns and provides appropriate responses.

---
+++ Sources/TelegramBot.ConsoleTest/Controllers/QueryController.cs
@@ -0,0 +1,21 @@
+﻿using TelegramBot.Attributes;
+using TelegramBot.Controllers;
+using TelegramBot.Abstractions;
+
+namespace TelegramBot.ConsoleTest.Controllers
+{
+    internal class QueryController : BotControllerBase
+    {
+        [TextQuery(pattern: ".+hello.+")]
+        public IActionResult HandleHelloAsync()
+        {
+            return Text("Hello!");
+        }
+
+        [TextQuery(pattern: "bye")]
+        public IActionResult HandleByeAsync()
+        {
+            return Text("Goodbye!");
+        }
+    }
+}
