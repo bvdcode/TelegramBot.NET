4603461a5d782daf0251afbf5e94cbffb6aaed67
Vadim Belov <github@belov.us>
2025-03-01 21:48:17 -0800

    Add logging mechanism to BotBuilder class
    Introduce a new logging mechanism to the `TelegramBot.Builders` namespace.
    Add a `Logging` property of type `ILoggingBuilder` to the `BotBuilder` class.
    Initialize `Logging` in the `BotBuilder` constructor with `TelegramLoggerBuilder`.
    Create `TelegramLoggerBuilder` class to manage logging services.
    Define `TelegramLoggerBuilder` in a new file, `TelegramLoggerBuilder.cs`.

---
+++ Sources/TelegramBot/Builders/TelegramLoggerBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TelegramBot.Builders
+{
+    internal class TelegramLoggerBuilder : ILoggingBuilder
+    {
+        public IServiceCollection Services { get; }
+
+        internal TelegramLoggerBuilder(IServiceCollection _services)
+        {
+            Services = _services ?? throw new ArgumentNullException(nameof(_services));
+        }
+    }
+}
